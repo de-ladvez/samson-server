aa44c33f5ac8a9a2c60bd0c4fc76c5a00391550e
Umnik <deladvez69@gmail.com>
2020-05-14 21:36:04 +0300

    change port

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ export const {
     // SESS_NAME = 'sid',
     // SESS_SECRET = 'secret!session',
     // SESS_LIFETIME = 1000 * 60 * 60 * 2,
-    MONGO_URI = 'mongodb+srv://samson:samson123654789@samson0-wzmpr.mongodb.net/samson?retryWrites=true&w=majority',
-    // MONGO_URI = 'mongodb://localhost:27017/samson' ,
+    // MONGO_URI = 'mongodb+srv://samson:samson123654789@samson0-wzmpr.mongodb.net/samson?retryWrites=true&w=majority',
+    MONGO_URI = 'mongodb://localhost:27017/samson' ,
 } = process.env;
